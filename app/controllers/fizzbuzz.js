70b1c60afd73aafbcb34657f3dc9ed750a46ae18
Steffi Pereira <steffipereira23@gmail.com>
2020-06-28 18:27:07 +0100

    created a list of numbers for fizzbuzz

--- app/controllers/fizzbuzz.js
+++ app/controllers/fizzbuzz.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 export default class FizzbuzzController extends Controller {
+  showNumbers = false;
+
+  get numbers() {
+    return Array.from({length: 50}, (_, index) => index + 1);
+  }
+
+  @action
+  fizzbuzz() {
+    //debugger;
+    this.toggleProperty('showNumbers');
+
+  }
 }
