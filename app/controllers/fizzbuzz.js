cbec50f2a34997203d30353322e426c1126bd0ce
Steffi Pereira <steffipereira23@gmail.com>
2020-07-04 23:54:08 +0100

    set a time for fizzbuzz game

--- app/controllers/fizzbuzz.js
+++ app/controllers/fizzbuzz.js
@@ -1,17 +1,36 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { htmlSafe } from '@ember/template';
 
 export default class FizzbuzzController extends Controller {
-  showNumbers = false;
-
-  get numbers() {
-    return Array.from({length: 50}, (_, index) => index + 1);
-  }
+  arr = Array(20).fill().map((_, index) => index + 1);
+  @tracked num;
+  @tracked numArr = [];
+  @tracked newStr = '';
 
   @action
   fizzbuzz() {
-    //debugger;
-    this.toggleProperty('showNumbers');
+    let timeout = 1000 ;
+    this.arr.forEach(el => {
+      if( el%15 === 0 ){
+        el = htmlSafe(`<span class="text-info">Fizzbuzz</span><em>(${el})</em>`)
+      }
+      if( el%5 === 0 ){
+        el = htmlSafe(`<span class="text-success">Buzz</span><em>(${el})</em>`)
+      }
+      if( el%3 === 0 ){
+        el = htmlSafe(`<span class="text-danger">Fizz</span><em>(${el})</em>`)
+      }
+      setTimeout(() => {
+        // debugger;
+        this.num = el;
+        this.numArr.pushObject(el);
+
+      }, timeout);
+      timeout += 1000;
+
+    })
 
   }
 }
